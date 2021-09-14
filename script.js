4d97ee6986b43164bb0309a2a8afe185aacd530d
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-09-14 10:05:58 -0300

    :100: Done?

--- script.js
+++ script.js
@@ -27,3 +27,52 @@ document.querySelectorAll('.header-section').forEach(function(item){
         }
     })
 })
+
+
+let caixa = document.getElementById('display-box')
+
+// ALL BORDERS
+function allBorder(degrees){
+    console.log(degrees)
+    document.querySelectorAll(".all-variable").forEach(function(item){
+        item.innerHTML = degrees + 'px';
+        caixa.style.borderRadius = degrees + 'px';
+    })
+}
+
+
+// 2X
+function diagonalBorders1(degrees){
+    caixa.style.borderTopLeftRadius = degrees + 'px';
+    caixa.style.borderBottomRightRadius = degrees + 'px';
+    document.getElementById('top-left').innerHTML = degrees + 'px';
+    document.getElementById('bottom-right').innerHTML = degrees + 'px';
+}
+
+function diagonalBorders2(degrees){
+    caixa.style.borderTopRightRadius = degrees + 'px';
+    caixa.style.borderBottomLeftRadius = degrees + 'px';
+    document.getElementById('top-right').innerHTML = degrees + 'px';
+    document.getElementById('bottom-left').innerHTML = degrees + 'px';
+}
+
+// 4X
+function topLeft(degrees){
+    caixa.style.borderTopLeftRadius = degrees + 'px';
+    document.getElementById('top-left').innerHTML = degrees + 'px';
+}
+
+function topRight(degrees){
+    caixa.style.borderTopRightRadius = degrees + 'px';
+    document.getElementById('top-right').innerHTML = degrees + 'px';
+}
+
+function botLeft(degrees){
+    caixa.style.borderBottomLeftRadius = degrees + 'px';
+    document.getElementById('bottom-left').innerHTML = degrees + 'px';
+}
+
+function botRight(degrees){
+    caixa.style.borderBottomRightRadius = degrees + 'px';
+    document.getElementById('bottom-right').innerHTML = degrees + 'px';
+}
