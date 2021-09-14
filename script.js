2eb885f646f2380e462980c01f8e1f882a531fb3
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-09-14 04:57:50 -0300

    :sparkles: working on

--- script.js
+++ script.js
@@ -8,13 +8,22 @@ document.querySelectorAll('.header-section').forEach(function(item){
         item.classList.toggle('active');
         
         if(item.id === 'header-section-1'){
-            
+            document.querySelector('#range-section-1').classList.add('active2');
+            document.querySelector('#range-section-1').classList.remove('inactive');
+            document.querySelector('#range-section-2').classList.add('inactive');
+            document.querySelector('#range-section-3').classList.add('inactive');
         }
         else if(item.id === 'header-section-2'){
-            
+            document.querySelector('#range-section-1').classList.add('inactive');
+            document.querySelector('#range-section-2').classList.add('active2');
+            document.querySelector('#range-section-2').classList.remove('inactive');
+            document.querySelector('#range-section-3').classList.add('inactive');
         }
         else if(item.id === 'header-section-3'){
-            
+            document.querySelector('#range-section-1').classList.add('inactive');
+            document.querySelector('#range-section-2').classList.add('inactive');
+            document.querySelector('#range-section-3').classList.remove('inactive');
+            document.querySelector('#range-section-3').classList.add('active2');
         }
     })
 })
